037515604951fb069ea1e49f13686ba8ccde89a7
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2018-08-02 20:52:15 -0300

    Remove comments from two_fer solved exercise

--- exercism/go/two-fer/two_fer.go
+++ exercism/go/two-fer/two_fer.go
@@ -1,16 +1,8 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package twofer should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Twofer package has only one function responsible to create a sentence of the form "One for X, one for me."
 package twofer
 
-// ShareWith should have a comment documenting it.
+// ShareWith create a sentence of the form "One for X, one for me."
 func ShareWith(name string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
 	if name == "" {
 		name = "you"
 	}
