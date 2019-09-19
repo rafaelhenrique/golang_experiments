6d0414a828fa4c1d0bc0f2e1468277b41d9dde96
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2019-09-19 17:05:10 -0300

    Add a better version of luhn (exercism)
    BenchmarkValid-8   	20000000	        64.3 ns/op	       0 B/op	       0 allocs/op

--- exercism/go/luhn/luhn.go
+++ exercism/go/luhn/luhn.go
@@ -1,52 +1,74 @@
 package luhn
 
 import (
-	"strings"
 	"unicode"
 )
 
 // Valid given a creditCardNumber calculate Luhn algorithm and returns if this is a valid number or not
 func Valid(creditCardNumber string) (isValid bool) {
 
-	// validate and sanitize sequence of numbers
-	var sb strings.Builder
-	sb.Grow(len(creditCardNumber))
-	for _, r := range creditCardNumber {
-		if unicode.IsNumber(r) {
-			sb.WriteRune(r)
-		} else if unicode.IsSpace(r) {
-			continue
-		} else {
-			return false
-		}
-	}
-	sanitizedCardNumber := sb.String()
+	// fmt.Println("=> start: credit card number", creditCardNumber)
 
 	// validate only one number
-	if len(sanitizedCardNumber) == 1 {
+	if len(creditCardNumber) == 1 {
 		return false
 	}
 
-	var sum, fNumber, sNumber int
-	for first, second := len(sanitizedCardNumber)-1, len(sanitizedCardNumber)-2; first >= 0; first, second = first-2, second-2 {
-
-		// logic to every second number from right to left
-		if second >= 0 {
-			sNumber = int(sanitizedCardNumber[second] - '0')
-			sNumber *= 2
-			if sNumber > 9 {
-				sNumber -= 9
-			}
-			sum += sNumber
+	var sum int
+	counter := 0
+	for i := len(creditCardNumber) - 1; i >= 0; i-- {
+		// every first digit from right to left
+		r := rune(creditCardNumber[i])
+		if unicode.IsDigit(r) {
+			// fmt.Println("F number", string(rune(creditCardNumber[i])))
+			sum += int(creditCardNumber[i] - '0')
+			counter++
+		} else if unicode.IsSpace(r) {
+			// fmt.Println("F space")
+			counter++
+		} else {
+			return false
 		}
 
-		fNumber = int(sanitizedCardNumber[first] - '0')
-		sum += fNumber
+		if i-1 < 0 {
+			continue
+		}
+
+		// every second digit from right to left
+		r = rune(creditCardNumber[i-1])
+		if unicode.IsDigit(r) {
+			if counter%3 == 0 {
+				continue
+			}
+			number := int(creditCardNumber[i-1] - '0')
+			number *= 2
+			if number > 9 {
+				number -= 9
+			}
+			sum += number
+			// fmt.Println("S number", number)
+			i--
+			counter++
+		} else if unicode.IsSpace(r) {
+			// fmt.Println("S space")
+			counter++
+		} else {
+			return false
+		}
+		// fmt.Println("-> Counter", counter)
 	}
 
+	// validate if credit card is " 0"
+	if unicode.IsSpace(rune(creditCardNumber[0])) && sum == 0 {
+		return false
+	}
+
+	// make operation to check if sum is divisible by 10
 	if sum%10 == 0 {
+		// fmt.Println("=> end: sum", sum)
 		return true
 	}
 
+	// fmt.Println("=> end: sum", sum)
 	return false
 }
